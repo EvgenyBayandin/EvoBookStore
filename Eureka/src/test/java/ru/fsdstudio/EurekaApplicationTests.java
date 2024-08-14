55d825fd3eff29e38f581f8633a3b63001623139
Evgeny Bayandin <5087081@gmail.com>
2024-08-14 18:40:06 +0300

    Init commit, add Eureka service

---
+++ Eureka/src/test/java/ru/fsdstudio/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.fsdstudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
